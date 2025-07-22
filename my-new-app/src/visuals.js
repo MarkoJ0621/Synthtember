88284d631e0e5ac0f414866f88c35894f24db13c
MarkoJ0621 <markolemesos@gmail.com>
2025-07-22 14:20:27 +0300

    another visual

--- my-new-app/src/visuals.js
+++ my-new-app/src/visuals.js
@@ -10,20 +10,44 @@ export function run(skew, handCount) {
     if (handCount > 0 && handCount <= 1) {
         osc(5, 0.9, () => Math.sin(time * 0.3))
             .colorama(skew)
+            .modulate(morphingRepeat(), 0.1)
+            .modulateRotate(o0, () => Math.sin(time * 0.2))
+            .out()
+        let targetRepeat = 1
+        let smoothRepeat = 1
+        setInterval(() => {
+            targetRepeat = Math.floor(Math.random() * 4) + 1
+        }, 500)
+
+        setInterval(() => {
+            smoothRepeat += (targetRepeat - smoothRepeat) * 0.003
+        }, 16)
+        function morphingRepeat() {
+            return gradient(2)
+                .colorama(1)
+                .kaleid(3)
+                .repeat(() => smoothRepeat, () => smoothRepeat, 0, 0)
+        }
+    } else if (handCount > 1 && handCount <= 4) {
+
+
+        osc(5, 0.9, () => Math.sin(time * 0.3))
+            .colorama(skew)
             .modulateRotate(o0, () => Math.sin(time * 0.2) * 3.5)
             .modulatePixelate(noise().pixelate(10), 16, 500).layer(src(s0).contrast(1.6).luma(0.5, 0.2))
             .modulateHue(o0, handCount)
             .out()
-    } else if (handCount > 1 && handCount <= 5) {
+    } else if (handCount > 4 && handCount <= 7) {
         src(s0).thresh(0.4).luma(0.2).brightness(-0.5).out(o1)
 
-        voronoi(10).modulateRepeat(o2, 10, 10, 1, 1).color(1, 0, 0.2, 1).modulate(src(o2).modulatePixelate(noise(10).pixelate(10), 10)).brightness(-0.4).out(o2)
+        voronoi(10).modulateRepeat(o2, 10, 10, 1, 1).color(1, 0, 0.2, 1).modulate(src(o2).modulatePixelate(noise(10).pixelate(10), 10)).brightness(-0.4).colorama(skew).out(o2)
 
         src(o2).layer(src(o1))
             .modulatePixelate(
                 noise(3, 0).pixelate(16, 16).
                     scroll(0, 0, 0.1, 0.4), () => Math.sin(0.2 * time) * 1024 * 0.65 + 300, 16)
-            .modulateHue(o2, () => Math.sin(0.4 * time) * 100)
+            .modulateHue(o2, () => Math.sin(0.4 * time) * 50)
             .out()
+
     }
 }
