3c1ec9ad2110a0560c3511e3fee45ff0da0b1538
MarkoJ0621 <markolemesos@gmail.com>
2025-08-25 00:32:58 -0400

    Revert "Reapply "idk im gna crash out""
    This reverts commit 74993d443f14a79df91515484af347e2548a734e.

--- my-new-app/webpack.renderer.config.js
+++ my-new-app/webpack.renderer.config.js
@@ -1,21 +1,17 @@
 const rules = require('./webpack.rules');
 const CopyPlugin = require('copy-webpack-plugin');
 
-// Add CSS rule to existing rules
 rules.push({
   test: /\.css$/,
   use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
 });
 
-// Add CSD rule to existing rules
-rules.push({
-  test: /\.csd$/,
-  type: 'asset/source'
-});
-
 module.exports = {
   module: {
-    rules: rules, // Use the extended rules array
+    rules: [{
+      test: /\.csd$/,
+      type: 'asset/source'
+    }]
   },
   plugins: [
     new CopyPlugin({
