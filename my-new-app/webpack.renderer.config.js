ef1aaa89cbce4fed4eb4f1aef393c2fba9b39a39
MarkoJ0621 <markolemesos@gmail.com>
2025-08-24 21:05:41 -0400

    idk im gna crash out

--- my-new-app/webpack.renderer.config.js
+++ my-new-app/webpack.renderer.config.js
@@ -1,17 +1,21 @@
 const rules = require('./webpack.rules');
 const CopyPlugin = require('copy-webpack-plugin');
 
+// Add CSS rule to existing rules
 rules.push({
   test: /\.css$/,
   use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
 });
 
+// Add CSD rule to existing rules
+rules.push({
+  test: /\.csd$/,
+  type: 'asset/source'
+});
+
 module.exports = {
   module: {
-    rules: [{
-      test: /\.csd$/,
-      type: 'asset/source'
-    }]
+    rules: rules, // Use the extended rules array
   },
   plugins: [
     new CopyPlugin({
